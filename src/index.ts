240a57c947b42d8eba9e0035d7b76aa695d73fdb
bijinfeng <2804673379@qq.com>
2026-03-17 11:14:41 +0800

    chore(package): bump version to 0.0.5 and add elementDialog & elementDrawer utils
    - Updated package version from 0.0.4 to 0.0.5
    - Imported elementDialog and elementDrawer from utils in index.ts
    - Exported create, hide, register, unregister, elementDialog, and elementDrawer in default export object
    - Reorganized exports for better API consistency

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { useModal } from "./hooks";
 import { Provider } from "./provider";
-import { antdDrawer, antdModal, create, hide, register, remove, show, unregister } from "./utils";
+import {
+	antdDrawer,
+	antdModal,
+	create,
+	elementDialog,
+	elementDrawer,
+	hide,
+	register,
+	remove,
+	show,
+	unregister,
+} from "./utils";
 
 export default {
-	useModal,
 	Provider,
+	useModal,
 	create,
 	hide,
 	register,
@@ -13,6 +24,8 @@ export default {
 	unregister,
 	antdModal,
 	antdDrawer,
+	elementDialog,
+	elementDrawer,
 };
 
 export type { NiceModalAction, NiceModalState, NiceModalStore } from "./store";
